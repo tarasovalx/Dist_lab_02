33689f0e5f2d0266b2b5c8abb5d359a0152f0218
tarasovalx <tarasovalx99@gmail.com>
2021-10-23 02:57:48 +0300

    Scripted auto-commit on change (2021-10-23 02:57:48) by gitwatch.sh

---
+++ launch.sh
@@ -0,0 +1,15 @@
+mvn -X package
+
+hadoop fs -rm airports.csv
+hadoop fs -rm flights.csv
+hdfs dfs -rm -r output
+
+hadoop fs -copyFromLocal airports.csv
+hadoop fs -copyFromLocal flights.csv
+
+export HADOOP_CLASSPATH=target/lab_02-1.0-SNAPSHOT.jar
+
+rm output -r
+
+hadoop FlightsJoinApp flights.csv airports.csv output
+hadoop fs -copyToLocal output
