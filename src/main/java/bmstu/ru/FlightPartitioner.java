f603da9ec85cf592a2bf1793621a9d69f84322eb
tarasovalx <tarasovalx99@gmail.com>
2021-10-23 10:30:10 +0300

    Scripted auto-commit on change (2021-10-23 10:30:07) by gitwatch.sh

--- src/main/java/bmstu/ru/FlightPartitioner.java
+++ src/main/java/bmstu/ru/FlightPartitioner.java
@@ -1,11 +1,10 @@
 package bmstu.ru;
 
-import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.mapreduce.Partitioner;
 
-public class FlightPartitioner extends Partitioner<AirportWritableComparable, FloatWritable> {
+public class FlightPartitioner extends Partitioner<AirportWritableComparable, FlightWritable> {
     @Override
-    public int getPartition(AirportWritableComparable key, FloatWritable val, int i) {
+    public int getPartition(AirportWritableComparable key, FlightWritable val, int i) {
         return key.getId() % i;
     }
 }
